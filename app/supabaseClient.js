18521f11d3e7fe4df0cef45d27251fc1f3ded46e
Jonathan Mahrt Guyou <jonathanmahrtguyou@gmail.com>
2025-02-05 20:12:38 +0100

    Initial commit

---
+++ app/supabaseClient.js
@@ -0,0 +1,8 @@
+// supabaseClient.js
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://uyfznzybyxzkvbounwop.supabase.co'
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
